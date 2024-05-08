a91d79e408e7b7948bf7c4e5559dc76bc8fef77a
RoimerMaestre <roimer.maestre93@gmail.com>
2024-05-07 22:06:39 -0400

    se agrega ventana modal y toolstip a los icon de la parte de abajo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,19 @@
-// Este código asegura que el script se ejecute después de que el documento HTML esté completamente cargado
-$(document).ready(function () {
-    // Seleccionamos todos los enlaces que empiezan con '#' y añadimos un evento de clic a cada uno
-    $('a[href^="#"]').on("click", function (e) {
-      e.preventDefault(); // Prevenimos el comportamiento por defecto del enlace (navegar a otra página)
-  
-      // Obtenemos el ID del elemento al que queremos hacer scroll suave
-      const targetId = $(this).attr("href").slice(1);
-  
-      // Buscamos el elemento en el DOM utilizando su ID
-      const targetElement = $("#" + targetId);
-  
-      // Verificamos si el elemento existe en la página
-      if (targetElement.length) {
-        // Si existe, animamos el scroll del documento HTML y el body
-        $("html, body").animate(
-          {
-            scrollTop: targetElement.offset().top, // Hacemos scroll hasta la posición superior del elemento
-          },
-          500
-        ); // Duración de la animación en milisegundos
-      }
-    });
-    //inicio en jquery para toolstip para no perderme
-    $('[data-bs-toggle="tooltip"]').tooltip();
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+});
+
+  //codigo para activar ventana modal
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+//cierre de codigo bootstrap-ventana modal
+
+
+
+
   
