dc521130a2cba6318c1f29308ad28e58a555f34b
RoimerMaestre <roimer.maestre93@gmail.com>
2024-05-08 19:27:34 -0400

    se agregan bordes abajo de los h2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+//toolstip
 document.addEventListener('DOMContentLoaded', function () {
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
