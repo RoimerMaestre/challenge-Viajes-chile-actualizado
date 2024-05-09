07779e06bf154f0644449b5de7c4808e4cac3963
RoimerMaestre <roimer.maestre93@gmail.com>
2024-05-08 20:50:32 -0400

    se modifica detalles

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# se trabaja en pantalla 1280*800 y en pantallas pequeñas se usa iphne 5/se.
+# ojo se trabaja en pantalla 1280*800 y en pantallas pequeñas se usa iphne 5/se.(320*568)
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,30 @@
 //toolstip
-document.addEventListener('DOMContentLoaded', function () {
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+document.addEventListener("DOMContentLoaded", function () {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
 });
 
-  //codigo para activar ventana modal
-  document.addEventListener('DOMContentLoaded', () => {
-    const myModal = document.getElementById('myModal');
-    const myInput = document.getElementById('myInput');
-  
-    if (myModal) {
-      myModal.addEventListener('shown.bs.modal', () => {
-        myInput.focus();
-      });
-    }
-  });
+//codigo para activar ventana modal
+document.addEventListener("DOMContentLoaded", () => {
+  const myModal = document.getElementById("myModal");
+  const myInput = document.getElementById("myInput");
 
-
-  //codigo de jquery para ventana emergente en boton enviar
-  $(document).ready(function(){
-    // Agregar controlador de eventos al hacer clic en el botón
-    $("#submitButton").click(function(){
-      // Mostrar mensaje de alerta
-      alert("¡Tu mensaje fue enviado exitosamente!");
+  if (myModal) {
+    myModal.addEventListener("shown.bs.modal", () => {
+      myInput.focus();
     });
+  }
+});
+
+//codigo de jquery para ventana emergente en boton enviar
+$(document).ready(function () {
+  // Agregar controlador de eventos al hacer clic en el botón
+  $("#submitButton").click(function () {
+    // Mostrar mensaje de alerta
+    alert("¡Tu mensaje fue enviado exitosamente!");
   });
-
-
-
-
-  
+});
